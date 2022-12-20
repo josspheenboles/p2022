ab0eb7ef058ffb14cd8ca5d80fd8fdcbba7c69c5
josspheenboles <eng.jospheenboles@gmail.com>
2022-12-20 12:36:43 +0200

    Day1

---
+++ Day1/day1demo.py
@@ -0,0 +1,84 @@
+name=input('enter name')
+print(name,type(name))
+
+age=int(input('enter age'))
+print((age)+10,type(age))
+'''
+print(max(1,2))
+print(min(1,2,56,1000.4787,7485745,-1))
+
+x,y,z=1,'ali'
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+name='ahmed'
+print(name[4:2:-1])#4,3
+print(name[::-1])
+
+print(name[0])
+print(name[0:3])#ahm
+print(name[2:4])
+print(name[2:])
+print(name[:2])
+print(name[4:2:-1])
+print(name[-1])
+
+
+name='{} {}'
+
+print(name.format('ahmed','ali'))
+print(name.format('mark','magdy'))
+error='{msg} {date} {msg}'
+
+print(error.format(msg='dfkdfjslkdjfkdlf',date='12-20'))
+print(error.format(date='12-20',msg='fjdjfljsd'))
+
+print(name.find('$'))
+print(name.split('$'))
+print(name.replace('$',''))
+print(name)
+
+#+, * ,index
+fname='ahmed'
+lname='ali'
+fullname=fname+lname+' mohamed'
+print(fullname)
+print(fname*5)
+
+x="ali ' "
+print(type(x))
+x='ahmed " '
+print(type(x))
+
+print(1==2)#equal in value & data type (int)
+print(1=='1')#equal value & not in data type
+print('asd' and 2) #True and True
+print('' and 'dd')
+
+print('====','' and 'dd','===')#True and False
+
+x=1==2
+print(1<2)
+
+x='1.1'
+#print(int(x))
+print(float(x))
+
+#loosly data
+x=1
+print(x)
+print(type(x))
+x=1.1
+print(x)
+print(type(x))
+x='ali'
+print(x)
+print(type(x))
+x=True
+print(x)
+print(type(x))
+_x=1
+x1=1
+'''
+
